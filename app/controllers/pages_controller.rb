a85e301d57621c4e30a34cf09a550822a7144c86
Ope <ope.bukola@gmail.com>
2012-10-11 14:02:31 -0400

    about and jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   def home
   	@user = User.new
   end
+  def about
+  	
+  end
+
+  def careers
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Launch::Application.routes.draw do
   root to: "pages#home"
   resources :users
+  match '/about', to: 'pages#about'
+  match '/careers', to: 'pages#careers'
 end
